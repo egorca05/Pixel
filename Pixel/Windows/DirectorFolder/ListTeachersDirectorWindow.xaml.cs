f97927c6c7dd5bbaf777859c9952ab8c0a0090ae
Егор Гапеев <gapeevegor@gmail.com>
2023-05-25 14:45:58 +0300

    Начал делать директора

--- Pixel/Windows/DirectorFolder/ListTeachersDirectorWindow.xaml.cs
+++ Pixel/Windows/DirectorFolder/ListTeachersDirectorWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Pixel.ClassFolder;
+using Pixel.FolderData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,34 +26,73 @@ namespace Pixel.Windows.DirectorFolder
             InitializeComponent();
         }
 
+        private void Ref()
+        {
+            TeacherData.ItemsSource = DBEntities.GetContext().User.ToList()
+                .OrderBy(c => c.IdUser);
+            //СДЕЛАТЬ ЧТОБЫ БЫЛИ ТОЛЬКО УЧИТЕЛЯ
+        }
+
         private void backBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            MenuDirectorWindow menuDirectorWindow = new MenuDirectorWindow();
+            menuDirectorWindow.Show();
+            this.Close();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            ClassMB.MBExit();
         }
 
         private void MoreBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            EditTeacherDirectorWindow editTeacherDirectorWindow = new EditTeacherDirectorWindow();  
+            editTeacherDirectorWindow.Show();
+            this.Close();
         }
 
         private void AddBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            AddTeacherDirectorWindow addTeacherDirectorWindow1 = new AddTeacherDirectorWindow();
+            addTeacherDirectorWindow1.Show();
+            this.Close();
         }
 
         private void DelBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (TeacherData.SelectedItem == null)
+            {
+                ClassMB.MBerror("Выберите адресс для удаления");
+            }
+            else
+            {
+                try
+                {
+                    User user = TeacherData.SelectedItem as User;
+                    if (ClassMB.QuestionMessage($"Удалить выбранного пользователя?"))
+                    {
+                        DBEntities.GetContext().User.Remove(user);
+                        DBEntities.GetContext().SaveChanges();
+                        Ref();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ClassMB.MBerror(ex);
+                }
+            }
         }
 
         private void ExitProfile_Click(object sender, RoutedEventArgs e)
         {
-
+            bool resultMB = ClassMB.QuestionMessage("Вы действительно хотите выйти из аккаунта?");
+            if (resultMB == true)
+            {
+                AutorizationWindow autorizationWindow = new AutorizationWindow();
+                autorizationWindow.Show();
+                this.Close();
+            }
         }
     }
 }
---
+++ Pixel/Windows/DirectorFolder/MenuDirectorWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Pixel.ClassFolder;
+using Pixel.Windows.DirectorFolder;
+
+namespace Pixel.Windows.DirectorFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuDirectorWindow.xaml
+    /// </summary>
+    public partial class MenuDirectorWindow : Window
+    {
+        public MenuDirectorWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClassMB.MBExit();
+        }
+
+        private void TeacherBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ListTeachersDirectorWindow listTeachersDirectorWindow = new ListTeachersDirectorWindow();
+            listTeachersDirectorWindow.Show();
+            this.Close();
+        }
+
+        private void GroupsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            GroupsDirectorWindow groupsDirectorWindow = new GroupsDirectorWindow();
+            groupsDirectorWindow.Show();
+            this.Close();
+        }
+
+        private void ResultBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ResultTestDirectorWindow resultTestDirectorWindow = new ResultTestDirectorWindow();
+            resultTestDirectorWindow.Show();
+            this.Close();
+        }
+
+        private void ExitProfile_Click(object sender, RoutedEventArgs e)
+        {
+            AutorizationWindow autorizationWindow = new AutorizationWindow();
+            autorizationWindow.Show();
+            this.Close();
+        }
+    }
+}
